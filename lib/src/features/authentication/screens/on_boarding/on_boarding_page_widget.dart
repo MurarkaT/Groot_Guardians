93d3bf4b45b298f09320e80cfef163505b206600
MurarkaT <tanishamurarka987@gmail.com>
2023-11-14 13:10:37 +0545

    Features: 3 Onboarding Slides added

---
+++ lib/src/features/authentication/screens/on_boarding/on_boarding_page_widget.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../../../constants/sizes.dart';
+import '../../models/model_on_boarding.dart';
+
+class OnBoardingPageWidget extends StatelessWidget {
+  const OnBoardingPageWidget({
+    Key? key,
+    required this.model,
+  }) : super(key: key);
+
+  final OnBoardingModel model;
+
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      padding: const EdgeInsets.all(tDefaultSize),
+      color: model.bgColor,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          //Image.network(model.image,alignment: Alignment.topRight),
+          Image(
+            image: AssetImage(model.image),
+            height: size.height * 0.45,
+          ),
+          Column(
+            children: [
+              Text(
+                model.title,
+                style: GoogleFonts.shadowsIntoLightTwo(textStyle: TextStyle(color: Colors.black,fontSize: 33,height: 1.0)),),
+              Text(
+                model.subTitle,
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+          Text(
+            model.counterText,
+            style: Theme.of(context).textTheme.headline6,
+          ),
+          const SizedBox(
+            height: 80.0,
+          )
+        ],
+      ),
+    );
+  }
+}
